a5f66b197725d6c59938d9ef14e7a63803238bd3
Real-Tract <83856804+Real-Tract@users.noreply.github.com>
2021-08-19 19:30:13 +0530

    Add files via upload

---
+++ midhun.js
@@ -0,0 +1,12 @@
+function mblk(){
+    document.body.style.backgroundColor="black";
+}
+function mgrn(){
+    document.body.style.backgroundColor="green";
+}
+function myell(){
+    document.body.style.backgroundColor="rgb(148, 148, 5)";
+}
+function mcyn(){
+    document.body.style.backgroundColor="rgb(7, 163, 163)";
+}
